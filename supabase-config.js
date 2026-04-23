a5fef709efb3af4a04f21723e9c21b842d7f1ae8
sasiskablen <135525220+sasiskablen@users.noreply.github.com>
2026-04-23 16:21:40 +0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.__SUPABASE_CONFIG__ = {
+  url: "https://hmzlaodhzewptdcycqvq.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhtemxhb2RoemV3cHRkY3ljcXZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NDAyMjgsImV4cCI6MjA5MjUxNjIyOH0.MekcRg5GpOYJVgqf6Hf7r4m9uIs-8oGj-CLck1vZjyM",
+};
